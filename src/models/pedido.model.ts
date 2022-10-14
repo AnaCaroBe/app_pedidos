11dc4df187c2f2736f889b573f5525907e8a9326
Ana Bedoya Cardona <ancarbedoya18@gmail.com>
2022-10-14 09:12:58 -0500

    version 1.0

---
+++ src/models/pedido.model.ts
@@ -0,0 +1,59 @@
+import {belongsTo, Entity, hasOne, model, property} from '@loopback/repository';
+import {Persona} from './persona.model';
+import {Producto} from './producto.model';
+
+@model()
+export class Pedido extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id_producto?: string;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  cantidad: number;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  total: number;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  estado: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  id_persona: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  id: string;
+
+  @belongsTo(() => Persona)
+  personaId: string;
+
+  @hasOne(() => Producto)
+  producto: Producto;
+
+  constructor(data?: Partial<Pedido>) {
+    super(data);
+  }
+}
+
+export interface PedidoRelations {
+  // describe navigational properties here
+}
+
+export type PedidoWithRelations = Pedido & PedidoRelations;
